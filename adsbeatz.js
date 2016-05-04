9c583612febd92b90e625be3f1a10302d7c4d643
suryadewa <suryadewa@users.noreply.github.com>
2016-05-05 06:08:43 +0800

    Update and rename adsbeatz.js to adsbeatz1.js

--- adsbeatz.js
+++
@@ -1,4 +0,0 @@
-if (document.getElementById("testadblock") == undefined)
-{
-document.getElementById('addbeatz').innerHTML="<center><p><font color='red'>Please disable your Ad Blocker </font>and <a href=''>refresh this page</a> to see the Button.</p></center>";
-}
