07374a2e9119b8efee782bc788f67913c4123430
suryadewa <suryadewa@users.noreply.github.com>
2016-05-05 05:01:23 +0800

    Create adbeatz.js

---
+++ adbeatz.js
@@ -0,0 +1,4 @@
+if (document.getElementById("testadblock") == undefined)
+{
+document.getElementById('main-content').innerHTML="<p>Please disable your Ad Blocker and <a href=''>refresh this page</a> to see the Button.</p>";
+}
