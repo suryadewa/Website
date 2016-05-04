3880f8d25a2fdfcd76a63e2510582a983a658752
suryadewa <suryadewa@users.noreply.github.com>
2016-05-04 09:11:07 -0700

    Update and rename adbs.js to adsbk.js

--- adsbk.js
+++ adsbk.js
@@ -31,7 +31,13 @@ function fnsantiy() {
     jQuery(function () {
         jQuery(document).ready(function () {
             setTimeout("Checking();", 1000);
-            jQuery("body").append('<div id="alert"><a href="https://4.bp.blogspot.com/-IAIoqRWeTgE/VyoUMHKXPDI/AAAAAAAAIl8/oJqZWnwew_A__FXuxGzs7IS1hTd_2DY0wCLcB/s1600/Stop%2BAdBlock.png"><img border="0" height="125" src="https://4.bp.blogspot.com/-IAIoqRWeTgE/VyoUMHKXPDI/AAAAAAAAIl8/oJqZWnwew_A__FXuxGzs7IS1hTd_2DY0wCLcB/s200/Stop%2BAdBlock.png" width="200" /></a><br /><p><h2>ADBLOCK DETECTED</h2><br/><span>Please Disable Adblocker and Refresh (F5) to continue visiting This Site. <br/></span></p></div>');
+            jQuery("body").append('<div id="alert"><a href="javascript:document.location.reload();"><img border="0" height="125" src="https://4.bp.blogspot.com/-IAIoqRWeTgE/VyoUMHKXPDI/AAAAAAAAIl8/oJqZWnwew_A__FXuxGzs7IS1hTd_2DY0wCLcB/s200/Stop%2BAdBlock.png" width="200" /></a><br /><p><h2>ADBLOCK DETECTED</h2><br/><span>Please Disable Adblocker and Refresh (F5) to continue visiting This Site. <br/>
+            <a href="javascript:document.location.reload();"
+ONMOUSEOVER="window.status='Refresh'; return true"
+ONMOUSEOUT="window.status='That was good'">
+<img src="https://4.bp.blogspot.com/-8NJOYgKlCxs/Vyoe6UIdUAI/AAAAAAAAImM/z05_Zdxp1qwGNl0_fDWz4JzaJuCG248OgCLcB/s1600/refresh.gif" 
+width="115" height="31" border="0" /></a>
+            </span></p></div>');
         });
     });
 }
