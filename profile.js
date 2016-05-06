80e93523d4cc5262d091dbf338cc7ba6c818c032
suryadewa <suryadewa@users.noreply.github.com>
2016-05-07 01:19:07 +0800

    Create profile.js

---
+++ profile.js
@@ -0,0 +1,4 @@
+if (document.getElementById("fnsadblock") == undefined)
+{
+document.getElementById('shower').innerHTML="<p><font color='red'>Please disable your Ad Blocker. </font>This website is funded by ads for keep alive <a href=''>Refresh</a></p>";
+}
