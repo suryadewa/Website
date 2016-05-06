1a6c7009e56fcb2c1e7950368ca9ad50a3e2f0de
suryadewa <suryadewa@users.noreply.github.com>
2016-05-07 02:48:17 +0800

    Update and rename profileku.js to profileq.js

--- profileku.js
+++
@@ -1,4 +0,0 @@
-if (document.getElementById("testadblock") == undefined)
-{
-document.getElementById('shower').innerHTML="<p><font color='red'>Please disable your Ad Blocker. </font>This website is funded by ads for keep alive <a href=''>Refresh</a></p>";
-}
---
+++ profileq.js
@@ -0,0 +1,4 @@
+if (document.getElementById("testadblock") == undefined)
+{
+document.getElementById('detectad').innerHTML="<p><font color='red'>Please disable your Ad Blocker. </font>This website is funded by ads for keep alive <a href=''>Refresh</a></p>";
+}
