7697509c212f861b2ef3ff8f630cc87c82d79f9b
suryadewa <suryadewa@users.noreply.github.com>
2016-05-07 02:58:45 +0800

    Update responsive.js

--- responsive.js
+++ responsive.js
@@ -1,4 +1,4 @@
 if (document.getElementById("testadblock") == undefined)
 {
-document.getElementById('detectad').innerHTML="<p><font color='red'>Please disable your Ad Blocker. </font>This website is funded by ads for keep alive <a href=''>Refresh</a></p>";
+document.getElementById('showtime').innerHTML="<p><font color='red'>Please disable your Ad Blocker. </font>This website is funded by ads for keep alive <a href=''>Refresh</a></p>";
 }
